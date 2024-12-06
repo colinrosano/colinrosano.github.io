80d52d3be0bd2c409ffbc4b99dcc9b9d48f10745
Colin Reinhardt <colinreinhardt@colin-osano-macbook-air.local>
2024-12-06 11:10:21 -0500

    added cookies

--- Scripts/analytics.js
+++ Scripts/analytics.js
@@ -1,1 +1,14 @@
 // this is an analytics script
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+  // Calculate expiration date
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+
+  // Set the cookie with name, value, and expiration
+  document.cookie = `${name}=${value}; ${expires}; path=/; SameSite=Lax`;
+}
+
+setCookie("analyticsCookie", "someData", 365);
--- Scripts/marketing.js
+++ Scripts/marketing.js
@@ -1,1 +1,14 @@
 // this is a marketing script
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+  // Calculate expiration date
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+
+  // Set the cookie with name, value, and expiration
+  document.cookie = `${name}=${value}; ${expires}; path=/; SameSite=Lax`;
+}
+
+setCookie("marketingCookie", "we love ads", 365);
