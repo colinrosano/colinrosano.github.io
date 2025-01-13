c62f36efc8ad9f1f6ce16dc9e4c535f5f833122f
Colin Reinhardt <colinreinhardt@colin-osano-macbook-air.local>
2025-01-13 16:57:16 -0500

    added modal to show consent uuid

--- Scripts/cookieDisply.js
+++ Scripts/cookieDisply.js
@@ -3,8 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const osano_consentmanager_uuid = localStorage.getItem(
     "osano_consentmanager_uuid"
   );
-  document.getElementById("cookie-display").textContent =
-    osano_consentmanager_uuid
-      ? `Your Consent ID: ${osano_consentmanager_uuid}`
-      : "No visitor ID found";
+
+  const modal = document.getElementById("consentModal");
+  const span = document.getElementsByClassName("close")[0];
+  const consentUUID = document.getElementById("consentUUID");
+
+  document.querySelector(".verifyConsent").onclick = function (event) {
+    event.preventDefault();
+    consentUUID.innerHTML = `To verify your consent, please reach out to us at privacy@company.com and provide us with your unique consent ID. <br> Your consent UUID is: ${osano_consentmanager_uuid}`;
+    modal.style.display = "block";
+  };
+
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 });
